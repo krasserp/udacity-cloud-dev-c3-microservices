029eef308f79c727a64d3ddbd4c7a53200402469
krasserp <krasserp@gmail.com>
2019-08-05 18:40:09 +0100

    refactor(microservice feed): remove and add needed files and functions for the feed service
    added auth to feed and removed user files and functions from feed service

--- udacity-c3-restapi-feed/src/controllers/v0/feed/routes/feed.router.ts
+++ udacity-c3-restapi-feed/src/controllers/v0/feed/routes/feed.router.ts
@@ -1,12 +1,36 @@
 import { Router, Request, Response } from 'express';
 import { FeedItem } from '../models/FeedItem';
-import { requireAuth } from '../../users/routes/auth.router';
+import { NextFunction } from 'connect';
+import * as jwt from 'jsonwebtoken';
 import * as AWS from '../../../../aws';
 import { isEmpty } from '../../../../helpers/commons';
+import { config } from '../../../../config/config';
 
 const HttpStatus = require('http-status-codes');
 
 const router: Router = Router();
+
+export function requireAuth(req: Request, res: Response, next: NextFunction) {
+  //   return next();
+  if (!req.headers || !req.headers.authorization) {
+    return res.status(401).send({ message: 'No authorization headers.' });
+  }
+
+  const token_bearer = req.headers.authorization.split(' ');
+  if (token_bearer.length != 2) {
+    return res.status(401).send({ message: 'Malformed token.' });
+  }
+
+  const token = token_bearer[1];
+  return jwt.verify(token, config.jwt.secret, (err, decoded) => {
+    if (err) {
+      return res
+        .status(500)
+        .send({ auth: false, message: 'Failed to authenticate.' });
+    }
+    return next();
+  });
+}
 
 interface parameterObject {
   caption?: string;
