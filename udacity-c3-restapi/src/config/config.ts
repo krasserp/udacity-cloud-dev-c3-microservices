ccccd929c901031a6d002142b50d72564e1241e8
krasserp <krasserp@gmail.com>
2019-08-05 17:52:59 +0100

    ci(environment): update environment variables
    update to local environment variables naming convention

--- udacity-c3-restapi/src/config/config.ts
+++ udacity-c3-restapi/src/config/config.ts
@@ -1,23 +1,22 @@
 export const config = {
-  "dev": {
-    "username": process.env.POSTGRESS_USERNAME,
-    "password": process.env.POSTGRESS_PASSWORD,
-    "database": process.env.POSTGRESS_DB,
-    "host": process.env.POSTGRESS_HOST,
-    "dialect": "postgres",
-    "aws_reigion": process.env.AWS_REGION,
-    "aws_profile": process.env.AWS_PROFILE,
-    "aws_media_bucket": process.env.AWS_BUCKET
+  dev: {
+    username: process.env.UG_POSTGRESS_USERNAME,
+    password: process.env.UG_POSTGRESS_PASSWORD,
+    database: process.env.UG_POSTGRESS_DBATABASE,
+    host: process.env.POSTGRESS_HOST,
+    dialect: 'postgres',
+    aws_reigion: process.env.UG_AWS_REGION,
+    aws_profile: process.env.UG_AWS_PROFILE,
+    aws_media_bucket: process.env.UG_AWS_BUCKET
   },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
+  prod: {
+    username: '',
+    password: '',
+    database: 'udagram_prod',
+    host: '',
+    dialect: 'postgres'
   },
-  "jwt": {
-    "secret": process.env.JWT_SECRET
+  jwt: {
+    secret: process.env.UG_JWT_SECRET
   }
-
-}
+};
